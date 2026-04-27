bdfb4e8c74bca1cabb1bbcc5ff43dda0ab0b217a
prithika1207 <prithikapradhaban0@gmail.com>
2026-04-27 16:17:41 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,27 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
+class FIFO:
+    def __init__(self, depth=4):
+        self.depth = depth
+        self.mem = []
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+    def write(self, data):
+        if len(self.mem) < self.depth:
+            self.mem.append(data)
+            return True
+        return False  # full
+
+    def read(self):
+        if len(self.mem) > 0:
+            return self.mem.pop(0)
+        return None  # empty
 
 
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+# -------- TEST --------
+fifo = FIFO(depth=4)
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+print("Write test:")
+for i in [10, 20, 30, 40, 50]:
+    print(i, "->", fifo.write(i))  # last one should fail
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+print("\nRead test:")
+for _ in range(5):
+    print(fifo.read())
